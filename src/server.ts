3f2c633083bbe5c9f2f8a366169df8f0b9d6949f
Igor Galindo <igorgalindopaiva@gmail.com>
2023-02-15 21:44:06 -0300

    environments variables

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
+import { env } from './env'
 import fastify from 'fastify'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions').select('*')
 
-  return tables
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
     host: '0.0.0.0',
   })
   .then(() => {
